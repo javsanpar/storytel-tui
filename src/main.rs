da14f5be414b4ee1f23424e0c1357bfbb55d04fd
Javier Sánchez Parra <javsanpar@riseup.net>
2020-05-18 19:35:50 +0200

    Add bookshelf request and audiobook selection
    This commit gets your bookshelf, prints it and let you choose a book
    from the bookshelf to listen.
    
    Co-authored-by: Roberto Garcia <9429166+robgc@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,28 @@ struct Login {
     account_info: AccountInfo,
 }
 
+#[derive(Deserialize)]
+struct BookShelf {
+    #[serde(rename = "books")]
+    books: Vec<BookEntry>,
+}
+
+#[derive(Deserialize)]
+struct BookEntry {
+    abook: Option<Abook>,
+    book: Book,
+}
+
+#[derive(Deserialize)]
+struct Abook {
+    id: u64,
+}
+
+#[derive(Deserialize)]
+struct Book {
+    name: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
 
@@ -44,17 +66,46 @@ async fn main() -> Result<(), reqwest::Error> {
         .build()
         .expect("should be able to build reqwest client");
 
-    let resp = client.get(url.as_str())
+    let resp_login = client.get(&url)
         .send()
         .await?;
-    println!("{:#?}", resp.status());
-    let resp_json = resp.json::<Login>()
+    println!("{:#?}", resp_login.status());
+    let login = resp_login.json::<Login>()
         .await?;
-    println!("{}", resp_json.account_info.jwt);
-    println!("{}", resp_json.account_info.single_sign_token);
+
+    let url_get_bookshelf = format!("https://www.storytel.com/api/getBookShelf.\
+                                    action?token={}",
+                                    login.account_info.single_sign_token);
+    let resp_bookshelf = client.get(&url_get_bookshelf)
+        .send()
+        .await?;
+    println!("{:#?}", resp_bookshelf.status());
+    let bookshelf = resp_bookshelf.json::<BookShelf>()
+        .await?;
+    for (i, bookentry) in bookshelf.books.iter().enumerate() {
+        match &bookentry.abook {
+            Some(abook) => println!("Index: {}\n{}", i, abook.id),
+            None    => continue,
+        }
+        println!("{}", bookentry.book.name);
+    }
+
+    println!("Please input desired index");
+    let mut index_string = String::new();
+    io::stdin()
+        .read_line(&mut index_string)
+        .expect("Failed to read index");
+    let index:usize = match index_string.trim().parse::<usize>() {
+        Ok(i) => i,
+        Err(..) => 0,
+    };
+
+    let id = bookshelf.books[index].abook.as_ref().unwrap().id;
+
     let url_ask_stream = format!("https://www.storytel.com/mp3streamRangeReq\
-                                 ?startposition=0&programId=754932&token={}",
-                                 resp_json.account_info.single_sign_token);
+                                 ?startposition=0&programId={}&token={}",
+                                 id,
+                                 login.account_info.single_sign_token);
     println!("{}", url_ask_stream);
 
     let resp = client.get(&url_ask_stream)
