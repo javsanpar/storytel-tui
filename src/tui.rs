4cc4406e16135dc6472c270e81fca99e6d97be5b
Roberto García Calero <9429166+robgc@users.noreply.github.com>
2020-07-22 14:16:38 +0200

    Format code
    Format the codebase using `rustfmt` (command: `cargo fmt`)
    with the default settings.

--- src/tui.rs
+++ src/tui.rs
@@ -1,18 +1,14 @@
-use cursive::views::{Dialog, EditView, TextView, LinearLayout, Button,
-                     SelectView};
 use cursive::traits::*;
+use cursive::views::{Button, Dialog, EditView, LinearLayout, SelectView, TextView};
 use cursive::Cursive;
 
 use crate::{client_storytel_api, mpv};
 
 fn show_player(siv: &mut Cursive, book_id: &u64) {
-    let client_data =
-        siv.user_data::<client_storytel_api::ClientData>().unwrap();
-    let url_ask_stream = client_storytel_api::get_stream_url(client_data,
-                                                             book_id);
+    let client_data = siv.user_data::<client_storytel_api::ClientData>().unwrap();
+    let url_ask_stream = client_storytel_api::get_stream_url(client_data, book_id);
 
-    let resp = client_data.request_client.get(&url_ask_stream)
-        .send();
+    let resp = client_data.request_client.get(&url_ask_stream).send();
 
     let location = resp.as_ref().unwrap().url().to_owned().into_string();
 
@@ -25,24 +21,21 @@ fn show_player(siv: &mut Cursive, book_id: &u64) {
 
 fn show_bookshelf(siv: &mut Cursive) {
     let bookshelf = client_storytel_api::get_bookshelf(
-        siv.user_data::<client_storytel_api::ClientData>().unwrap());
+        siv.user_data::<client_storytel_api::ClientData>().unwrap(),
+    );
     siv.pop_layer();
     let mut book_select: Vec<(String, u64)> = Vec::new();
     for book_entry in bookshelf.books.iter() {
         match &book_entry.abook {
-            Some(abook) => book_select.push((book_entry.book.name.clone(),
-                                             abook.id)),
-            None    => continue,
+            Some(abook) => book_select.push((book_entry.book.name.clone(), abook.id)),
+            None => continue,
         }
         println!("{}", book_entry.book.name);
     }
     let select = SelectView::new()
         .with_all(book_select.into_iter())
         .on_submit(show_player);
-    siv.add_layer(
-        Dialog::around(select.scrollable())
-            .title("Select a book to listen"),
-    );
+    siv.add_layer(Dialog::around(select.scrollable()).title("Select a book to listen"));
 }
 
 fn show_check_login(siv: &mut Cursive, email: &str, pass: &str) {
@@ -53,43 +46,39 @@ fn show_check_login(siv: &mut Cursive, email: &str, pass: &str) {
     } else {
         client_storytel_api::login(
             siv.user_data::<client_storytel_api::ClientData>().unwrap(),
-            email, pass);
+            email,
+            pass,
+        );
         siv.pop_layer();
-        siv.add_layer(Dialog::around(LinearLayout::vertical()
-                .child(Button::new("Bookshelf", show_bookshelf))
-                .child(Button::new("Exit", show_login)))
-            .title("Menu"));
+        siv.add_layer(
+            Dialog::around(
+                LinearLayout::vertical()
+                    .child(Button::new("Bookshelf", show_bookshelf))
+                    .child(Button::new("Exit", show_login)),
+            )
+            .title("Menu"),
+        );
     }
 }
 
 pub fn show_login(siv: &mut Cursive) {
-    siv.add_layer(Dialog::around(LinearLayout::vertical()
-            .child(TextView::new("Email"))
-            .child(
-                EditView::new()
-                    .with_name("email")
-                    .fixed_width(20)
-                )
-            .child(TextView::new("Password"))
-            .child(
-                EditView::new()
-                    .secret()
-                    .with_name("pass")
-                    .fixed_width(20)
-                ))
+    siv.add_layer(
+        Dialog::around(
+            LinearLayout::vertical()
+                .child(TextView::new("Email"))
+                .child(EditView::new().with_name("email").fixed_width(20))
+                .child(TextView::new("Password"))
+                .child(EditView::new().secret().with_name("pass").fixed_width(20)),
+        )
         .button("Ok", |s| {
-                let email = s
-                    .call_on_name("email", |view: &mut EditView| {
-                        view.get_content()
-                    })
-                    .unwrap();
-                let pass = s
-                    .call_on_name("pass", |view: &mut EditView| {
-                        view.get_content()
-                    })
-                    .unwrap();
-                show_check_login(s, &email, &pass);
+            let email = s
+                .call_on_name("email", |view: &mut EditView| view.get_content())
+                .unwrap();
+            let pass = s
+                .call_on_name("pass", |view: &mut EditView| view.get_content())
+                .unwrap();
+            show_check_login(s, &email, &pass);
         })
-        .title("Login"));
+        .title("Login"),
+    );
 }
-
